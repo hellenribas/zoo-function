d8c0a6b41a99ec41b54fbb6e3f88ea313149acc4
hellenribas <tchwtrabalho@gmail.com>
2022-01-25 19:06:58 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return data.employees
+    .find((elem) =>
+      elem.firstName === employeeName || elem.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
