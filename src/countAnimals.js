b4269d40760bb71a7e0c5e0424473e03a8e87288
hellenribas <tchwtrabalho@gmail.com>
2022-01-26 13:21:55 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
+const spec = (animal) => {
+  if (Object.keys(animal).length === 2) {
+    return ((data.species.filter((elem) =>
+      elem.name === (Object.values(animal))[0]))
+      .map((elem) => elem.residents.filter((gen) => gen.sex === (Object.values(animal))[1])))
+      .reduce((acc, elem) => acc + elem.length, 0);
+  }
+  return data.species.filter((elem) =>
+    elem.name === (Object.values(animal))[0])
+    .reduce((acc, elem2) => acc + ((elem2.residents).length), 0);
+};
 function countAnimals(animal) {
-  // seu código aqui
+  const obj = {};
+  if (animal === undefined) {
+    data.species.forEach((elem) => {
+      obj[elem.name] = (elem.residents).length;
+    });
+    return obj;
+  }
+  return spec(animal);
 }
 
 module.exports = countAnimals;
