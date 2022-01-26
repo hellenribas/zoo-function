38590fefd635b3bdabf941532d9c3d35683f2e25
hellenribas <tchwtrabalho@gmail.com>
2022-01-26 16:40:04 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,38 @@
 const data = require('../data/zoo_data');
 
+const obj = {};
+const count = (entrants) => {
+  obj.child = entrants.filter((elem) => elem.age < 18).length;
+  obj.adult = entrants.filter((elem) => elem.age >= 18 && elem.age < 50).length;
+  obj.senior = entrants.filter((elem) => elem.age >= 50).length;
+  return obj;
+};
 function countEntrants(entrants) {
-  // seu código aqui
+  return entrants.map((elem) => {
+    if (elem === undefined) {
+      return 0;
+    }
+    if (entrants.length === 0) {
+      return {};
+    }
+    return count(entrants);
+  })[0];
 }
-
+// console.log(countEntrants(array));
 function calculateEntry(entrants) {
-  // seu código aqui
+  let total = 0;
+  const objKey = Object.keys(countEntrants(entrants));
+  const value = Object.values(countEntrants(entrants));
+  objKey.map((elem) => {
+    if (elem === 'child') {
+      total += value[0] * 20.99;
+    } else if (elem === 'adult') {
+      total += value[1] * 49.99;
+    } else if (elem === 'senior') {
+      total += value[2] * 24.99;
+    }
+    return total;
+  });
+  return total;
 }
-
 module.exports = { calculateEntry, countEntrants };
