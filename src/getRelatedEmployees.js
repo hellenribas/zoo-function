67149146f3492afd88be3929639e87c2e6e7e8f4
hellenribas <tchwtrabalho@gmail.com>
2022-01-25 21:40:13 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((elem) => elem.managers.find((manag) => manag === id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  try {
+    if (isManager(managerId) === true) {
+      const colabMan = (data.employees.filter((elem) =>
+        elem.managers.some((elemMan) =>
+          elemMan === managerId)));
+      return colabMan.map((elem2) => `${elem2.firstName} ${elem2.lastName}`);
+    }
+  } catch (erro) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 }
-
+console.log(getRelatedEmployees());
 module.exports = { isManager, getRelatedEmployees };
